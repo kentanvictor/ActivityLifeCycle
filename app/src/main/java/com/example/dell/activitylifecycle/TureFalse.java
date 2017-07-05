2c27dc9977ded5de86fce715c7936d679385f1e9
kentanvictor <2030457926@qq.com>
2017-07-05 15:00:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/activitylifecycle/TureFalse.java
@@ -0,0 +1,33 @@
+package com.example.dell.activitylifecycle;
+
+/**
+ * Created by DELL on 2017/7/5.
+ */
+public class TureFalse {
+    private int mQuestion;
+    // this variable will hold a resource ID for a string
+
+    private boolean mTrueQuestion;
+
+    public TureFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+
+}
